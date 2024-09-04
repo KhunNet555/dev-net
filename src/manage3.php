067a033b66f54783316d8720ee5eef655a7fec4c
KhunNet <131509141+KhunNet555@users.noreply.github.com>
2024-09-04 22:39:46 +0700

    checkbox+deleteAPI

--- src/manage3.php
+++ src/manage3.php
@@ -30,7 +30,7 @@
                         <div>save</div>
                         <input type="checkbox" id="selectAllCheckbox"></input>
                         <label for="selectAllCheckbox">เลือกทั้งหมด</label>
-                        <button type="button">delete</button>
+                        <button data-button-delete="all" type="button" class="hidden">delete</button>
                     </div>
                 </div>
                 <table data-hw="hw">
@@ -56,14 +56,13 @@
         </div>
     </div>
 
-
     <script>
         const dataTable = document.querySelector(`[data-hw="hw"]`);
         const dataBody = document.querySelector("tbody");
+        const selectAllCheckbox = document.getElementById('selectAllCheckbox');
+        const deleteButton = document.querySelector(`[data-button-delete="all"]`);
 
         function dataTable2(info) {
-            console.log(info);
-            console.log(info.length);
             for (let i = 0; i < info.length; i++) {
                 const trCell = document.createElement("tr");
                 dataBody.appendChild(trCell);
@@ -110,11 +109,102 @@
 
                 checkCell.type = "checkbox";
 
+                // กำหนดค่าให้ Attribute
+                checkCell.setAttribute("data-delete", info[i].id)
+
                 trCell.appendChild(delCell);
                 delCell.appendChild(checkCell);
-
-                // delCell.innerHTML = "info";
             }
+        }
+
+        deleteButton.addEventListener("click", function() {
+            const checkboxes = document.querySelectorAll(`[data-delete]`);
+            let deleteList = [];
+
+            checkboxes.forEach(checkbox => {
+                if (checkbox.checked == true) {
+                    console.log(checkbox.getAttribute("data-delete"));
+                    deleteList.push(checkbox.getAttribute("data-delete"))
+                }
+            });
+            deleteData(deleteList);
+
+        })
+
+        function deleteData(list) {
+            console.log(list);
+
+            const myData = new FormData()
+            const type = "slide";
+
+            myData.append("listID", list)
+            myData.append("type", type)
+
+            const url = "../api/remove_api.php";
+
+            fetch(url, {
+                    method: 'POST',
+                    credentials: 'include',
+                    body: myData,
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok: ' + response.statusText);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        }
+
+        // อัพเดท checkbox
+        function updateCheckboxes(checked) {
+            const checkboxes = dataBody.querySelectorAll('input[type="checkbox"]');
+            checkboxes.forEach(checkbox => {
+                checkbox.checked = checked;
+                if (checked == true) {
+                    deleteButton.classList.remove("hidden");
+                }
+            });
+        }
+
+        selectAllCheckbox.addEventListener('change', function() {
+            updateCheckboxes(selectAllCheckbox.checked);
+        });
+
+        window.addEventListener("change", function() {
+            toggleDeleteButton();
+        })
+
+        // check status ทั้งหมด
+        function toggleDeleteButton() {
+            const checkboxes = document.querySelectorAll("[data-delete]");
+
+            let isAnyChecked = false;
+
+            checkboxes.forEach(checkbox => {
+                checkbox.addEventListener("click", function() {
+                    console.log(checkbox.checked);
+                    if (checkbox.checked == false) {
+                        selectAllCheckbox.checked = false
+                    }
+                });
+                if (checkbox.checked) {
+                    isAnyChecked = true;
+                }
+            });
+            if (isAnyChecked == true) {
+                // โชว์
+                deleteButton.classList.remove("hidden")
+            } else {
+                // ซ่อน
+                deleteButton.classList.add("hidden");
+            }
+            console.log(isAnyChecked);
         }
 
         function fetchData() {
